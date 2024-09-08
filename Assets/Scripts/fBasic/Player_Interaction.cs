9ccfff65e33b512e3e05e2744cccc80e861b2789
James Patrick Paloyo <payloyo@gmail.com>
2024-09-08 11:46:21 +0800

    Room_Template Shop
    -Shop location added

--- Assets/Scripts/fBasic/Player_Interaction.cs
+++ Assets/Scripts/fBasic/Player_Interaction.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class Player_Interaction : MonoBehaviour
@@ -9,15 +10,28 @@ public class Player_Interaction : MonoBehaviour
     //CURRENT TELEPORTER standing in
     private GameObject currentTpObject;
     public  Camera Camera;
+    public GameObject LoadScreen;
+    private float waitTime;
 
     public GameObject Optional_Obj;
     public void interact_Now()
     {
+        LoadScreen.SetActive(true);
+        while(waitTime != 2) {
+            waitTime += Time.deltaTime;
+        }
         if (currentTpObject != null)
-        {    
+        {   
+            //transfrom current player position INTO(=) current TP objbect Get component(Script) <ScriptName>(run) -Method- [.Get -type- Destination()] DOT -type-(position)
             transform.position = currentTpObject.GetComponent<Teleport>().GetDestination().position;
-            Camera.transform.position = currentTpObject.GetComponent<Teleport>().GetDestination().position;
+            //transform current player position into teleport Destination
 
+            //transfrom current camera position INTO(=) current TP objbect Get component(Script) <ScriptName>(run) -Method- [.Get -type- Destination()] DOT -type-(position)
+            Camera.transform.position = currentTpObject.GetComponent<Teleport>().GetDestination().position;
+            //follow player with camera
+
+            LoadScreen.SetActive(false);
+            //currentTpObject.SetActive(false);
         }
     }
 
@@ -25,6 +39,14 @@ public class Player_Interaction : MonoBehaviour
     {
         //verify if currently on teleporter
         if (collision.CompareTag("Teleporter"))
+        {
+            // if TRUE
+            currentTpObject = collision.gameObject;
+            Optional_Obj.SetActive(true);
+        }
+
+        // verify if infront of shop
+        if (collision.CompareTag("TeleporterToShop"))
         {
             // if TRUE
             currentTpObject = collision.gameObject;
@@ -42,4 +64,6 @@ public class Player_Interaction : MonoBehaviour
             Optional_Obj.SetActive(false);
         }
     }
+
+
 }
